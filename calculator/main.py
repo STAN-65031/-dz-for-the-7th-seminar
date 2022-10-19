273b26fe9a6118a78bd7e0f375d861fe5e0d99e8
STAN-65031 <lox.ibany@gmail.com>
2022-10-19 20:28:39 +0300

    start for menu

--- calculator/main.py
+++ calculator/main.py
@@ -1,3 +1,7 @@
 import controller as c
 
 c.button_click()
+
+# import menu
+
+# menu.grid()
---
+++ calculator/menu.py
@@ -0,0 +1,57 @@
+import model_sum as s1
+import model_dif as s2
+import model_multi as s3
+import model_division as s4
+import model_remainder_of_division as s5
+import model_pow as s6
+import model_integer_division as s7
+
+def grid (value_a, value_b):
+    while True:
+        print("Введите 0 что бы закрыть")
+        print("Введите 1 что бы сложить")
+        print("Введите 2 что бы вычесть")
+        print("Введите 3 что бы умножить")
+        print("Введите 4 что бы разделить")
+        print("Введите 5 что бы узнать остаток от деления")
+        print("Введите 6 что бы возвести в степень")
+        print("Введите 7 что бы произвести целочисленное деление")
+        def input_numbers(inputText):
+            okey = False
+            while not okey:
+                try:
+                    number = int(input(f"{inputText}"))
+                    okey = True
+                    if 0 > number < 8:
+                        okey = False
+                        print("'это число не подходит")
+                except ValueError:
+                    print("Какое-то неправильное число!")
+            return number
+
+        s = input_numbers("Выберите действие: ")
+        if s == 0:
+            break
+        if s == 1:
+            s1.init(value_a,value_b)
+            return s1.do_it()
+        elif s ==2:
+            s2.init(value_a,value_b)
+            return s2.do_it()
+        elif s == 3:
+            s3.init(value_a,value_b)
+            return s3.do_it()
+        elif s == 4:
+            s4.init(value_a,value_b)
+            return s4.do_it()
+        elif s == 5:
+            s5.init(value_a,value_b)
+            return s5.do_it()
+        elif s == 6:
+            s6.init(value_a,value_b)
+            return s6.do_it()
+        elif s == 7:
+            s7.init(value_a,value_b)
+            return s7.do_it()
+
+
